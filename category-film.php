cc72eb5466a047c91bf3df499bf3da99cdadda25
James Shakespeare <j@jshakespeare.com>
2013-06-11 14:23:41 +0100

    films category page, some styling mods on videos

---
+++ category-film.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+<div class="works wrapper">
+    <?php
+    
+    global $wp_query;
+
+    $args = array(
+        'post_type' => 'work',
+        'posts_per_page' => -1,
+        'orderby' => 'menu_order',
+        'order' => 'ASC'
+    );
+
+    if(isset($wp_query->query["cat"])){
+
+        $args["cat"] = $wp_query->query["cat"];
+    }
+    elseif(isset($wp_query->query["category_name"])){
+
+        $args["category_name"] = $wp_query->query["category_name"];
+    }
+
+    query_posts($args);
+
+    if(have_posts()):
+        while(have_posts()): 
+            the_post();
+            ?>
+            <article class="work video" id="video-<?php the_id(); ?>">
+                <h3><?php the_title(); ?></h3>
+                <?php the_content(); ?>
+            </article>
+            <?php
+        endwhile;
+    endif;
+    ?>
+</div>
+
+<p><a href="/">&larr; Back to all works</a></p>
+
+<?php get_footer(); ?>
